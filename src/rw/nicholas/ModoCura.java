3617708fd4e6be84c90e53d37a28c14bfa67e7da
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-07-03 11:18:37 -0300

    ModoCura
    Atualmente apenas paladino é curado, mas... tá ai para talvez futura implementação

---
+++ src/rw/nicholas/ModoCura.java
@@ -0,0 +1,18 @@
+package rw.nicholas;
+import rw.nicholas.personagens.Medico;
+import rw.nicholas.personagens.Paladino;
+
+public class ModoCura {
+	private Paladino paladino;
+	private Medico medico;
+	
+	public ModoCura(Paladino paladino, Medico medico) {
+		this.paladino = paladino;
+		this.medico = medico;
+	}
+	
+	public boolean curar() {
+		return medico.Curar(paladino);
+	}
+	
+}
