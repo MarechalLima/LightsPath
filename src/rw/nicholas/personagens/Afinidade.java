f73a718db82c68ba78c51132e4baeb63ce837466
Nícholas Lima de Souza Silva <nicholaslima.rw@gmail.com>
2019-07-03 10:56:04 -0300

    todo

--- src/rw/nicholas/personagens/Afinidade.java
+++ src/rw/nicholas/personagens/Afinidade.java
@@ -2,4 +2,5 @@ package rw.nicholas.personagens;
 
 public interface Afinidade {
 	public boolean matchAfinidade(Paladino paladino);
+	//TODO futuramente um aplicar teste de persuasão para talvez converter a afinidade do personagem
 }
